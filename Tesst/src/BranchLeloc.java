8e5d1dea2efcbdf98a0fd7fe43a9a0de8cbefadc
demogithubnlu <39187616+demogithubnlu@users.noreply.github.com>
2018-07-13 09:48:20 +0700

    BranchLeloc.java
    update by phamhuuquy

--- Tesst/src/BranchLeloc.java
+++ Tesst/src/BranchLeloc.java
@@ -2,7 +2,7 @@
 public class BranchLeloc {
 
 	public static void main(String[] args) {
-		System.out.println("demo brach by leloc");
+		System.out.println("demo brach by leloc and phamhuuquy");
 	}
 
 }
